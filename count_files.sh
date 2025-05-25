9520eceb4c8f9d514d015708f2244f8838beb4f6
hroun20182 <harounrachid0000@gmail.com>
2025-05-25 14:33:18 +0100

    c01

--- count_files.sh
+++ count_files.sh
@@ -2,11 +2,5 @@
 # Nom du dossier à analyser
 echo "Entrer le nom du dossier :"
 read folder
-# Vérifie si le dossier existe
-if [ -d "$folder" ]; then
-    # Compte le nombre de fichiers (en excluant les sous-dossiers)
-    count=$(ls -p "$folder" | grep -v / | wc -l)
-    echo "Le dossier $folder contient $count fichier(s)"
-else
-    echo "Le dossier $folder n'existe pas."
-fi
+# Compte le nombre de fichiers (pas de dossiers) dans le répertoire
+nb_fichiers=$(find "$dossier" -maxdepth 1 -type f | wc -l)
